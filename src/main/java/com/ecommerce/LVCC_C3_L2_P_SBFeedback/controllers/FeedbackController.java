2ab7f4113dc996c3cb32faa6224607a61ccab7d0
hirahul0011 <ra_10001@yahoo.co.in>
2023-10-09 00:29:50 +0530

    index and submissioConfirmation jsp pages finalized and respective controller redirect is added; Project is also finalized with some changes in formSubmission controlled

--- src/main/java/com/ecommerce/LVCC_C3_L2_P_SBFeedback/controllers/FeedbackController.java
+++ src/main/java/com/ecommerce/LVCC_C3_L2_P_SBFeedback/controllers/FeedbackController.java
@@ -1,7 +1,11 @@
 package com.ecommerce.LVCC_C3_L2_P_SBFeedback.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,11 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ecommerce.LVCC_C3_L2_P_SBFeedback.entity.Feedback;
 import com.ecommerce.LVCC_C3_L2_P_SBFeedback.repository.FeedbackSpringDataRepository;
 
-@RestController
+//@RestController
+@Controller
 public class FeedbackController {
 	
 	@Autowired
 	FeedbackSpringDataRepository feedbackSpringDataRepository;
+	
+//	@RequestMapping(value="/")
+//	@GetMapping(value="/")
+	public String start() {
+		return "index";
+	}	
 	
 	@RequestMapping(value="formSubmission",method=RequestMethod.POST)
 	public String formSubmission(@RequestParam("feedback")String feedback,
@@ -22,13 +33,39 @@ public class FeedbackController {
 			@RequestParam("source")String source,
 			ModelMap map) {
 		
-		String page="status";
+		String page="index";
+		String errorMessages="";
 		
-		Feedback fb=new Feedback(feedback, product, source);
-		feedbackSpringDataRepository.save(fb);
+		if(feedback=="") {			
+			errorMessages=errorMessages+"<br>Please enter some comment to continue!";			
+		}
+		
+		if(product=="") {			
+			errorMessages=errorMessages+"<br>Please enter Product Name to continue!";			
+		}
+		
+		if(source=="") {			
+			errorMessages=errorMessages+"<br>Please enter Source to continue!";			
+		}
+		
+		if(errorMessages=="") {				
+			Feedback fb=new Feedback(feedback, product, source);
+			feedbackSpringDataRepository.save(fb);	
+			page="submissioConfirmation";
+		}else {
+			map.addAttribute("errorMessages", errorMessages);	
+			map.addAttribute("feedback", feedback);			
+		}		
 		
 		return page;
 		
+	}
+	
+	@RequestMapping(value="redirect",method=RequestMethod.GET)	
+	public String redirect(){
+		
+		String page="index";				
+		return page;		
 	}
 
 }
---
+++ src/main/webapp/WEB-INF/jsp/submissioConfirmation.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<div align="center">	
+	<h1>Details updated in DataBase successfully!</h1>
+	<h2><a href="redirect">Form</a></h2>				
+	</div>
+</body>
+</html>
