60a91d34ed78ffe080a87fb8b33284cbc20bf878
hirahul0011 <ra_10001@yahoo.co.in>
2023-10-08 17:54:50 +0530

    LVCC_C3_L2_P_SBFeedback

---
+++ src/main/java/com/ecommerce/LVCC_C3_L2_P_SBFeedback/LvccC3L2PSbFeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.LVCC_C3_L2_P_SBFeedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LvccC3L2PSbFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LvccC3L2PSbFeedbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/LVCC_C3_L2_P_SBFeedback/LvccC3L2PSbFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.LVCC_C3_L2_P_SBFeedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LvccC3L2PSbFeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
